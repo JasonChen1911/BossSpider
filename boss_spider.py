6daa3cde9f418afdfb90505adee73d53e8511724
Jason <923927933@qq.com>
2018-11-08 15:53:54 +0800

    完善写出

--- boss_spider.py
+++ boss_spider.py
@@ -11,18 +11,25 @@
 import requests
 from bs4 import BeautifulSoup
 from settings import HEADERS
+import re
 
 class BossSpider(object):
-    def get_web(self, url):
+    def get_web(self, url, referer=None):
         session = requests.session()
-        response = session.get(url, headers = HEADERS)
-        return response
+        if(referer):
+            HEADERS['referer'] = referer
+        while (True):
+            try:
+                response = session.get(url, headers = HEADERS)
+                if response.status_code == 200:
+                    return response
+            except:
+                pass
+
 
     def get_menu(self):
         url = 'https://www.zhipin.com/?ka=header-home-logo'
         response = self.get_web(url)
-        print(response)
-        #print(response.text)
         soup = BeautifulSoup(response.text, 'lxml')
         div_soups = soup.find_all('div', class_='menu-sub')
         a_soups = div_soups[0].find_all('a')
@@ -30,16 +37,48 @@ class BossSpider(object):
             #print(a_soup['href'])
             #print(a_soup.text)
             pass
+    def write_csv(self, data):
+        with open('/mnt/jobs_detail.txt', 'a') as fp:
+            fp.write(data+'\n')
 
     def get_category_info(self, category):
         base_url = 'https://www.zhipin.com'
+        old_url = ''
         for index in range(1, 11):
-            url = base_url + category + '&page=' + str(index)
-            print(url)
-        url = 'https://www.zhipin.com/c101010100-p100101/&page=11'
-        reponse = self.get_web(url)
-        print(reponse)
-
+            if index == 1:
+                url = base_url + category
+                self.get_web(url)
+            else:
+                url = base_url + category + '&page=' + str(index) + "&ka=page-" + str(index)
+                self.get_web(url, old_url)
+            old_url = url
+            response = self.get_web(url)
+            soup = BeautifulSoup(response.text, 'lxml')
+            div_soup = soup.find_all('div', class_ = 'job-list')
+            li_soups = div_soup[0].find_all('li')
+            for li_soup in li_soups:
+                data = {}
+                info_soup = li_soup.find_all('div', class_='info-primary')[0]
+                data['job_id'] = info_soup.find_all('a')[0]['data-jid']
+                data['job_title'] = info_soup.find_all('div', class_='job-title')[0].text
+                data['job_salary'] = info_soup.find_all('span')[0].text
+                pattern = re.compile(r'\<p\>(.*)\<em class="vline"\>\<\/em\>(.*?)\<em class="vline"\>\<\/em\>(.*)\<\/p\>', re.S)
+                info_str = str(info_soup)
+                result = re.search(pattern, info_str)
+                data['job_addr'] = result.group(1)
+                data['job_age'] = result.group(2)
+                data['job_education'] = result.group(3)
+                company_soup = li_soup.find_all('div', class_='info-company')[0]
+                data['company_name'] = company_soup.find_all('h3')[0].text
+                pattern = re.compile(r'<p>(.*)<em class="vline"></em>(.*?)<em class="vline"></em>(.*)</p>', re.S)
+                company_str = str(company_soup)
+                result = re.search(pattern, company_str)
+                data['company_industry'] = result.group(1)
+                data['company_financing'] = result.group(2)
+                data['company_size'] = result.group(3)
+                str_data = data['job_id'] +'|'+ data['job_title'] +'|'+ data['job_salary'] +'|'+ data['job_addr'] +'|'+ data['job_age'] +'|'+ data['job_education'] \
+                           +'|'+ data['company_name'] +'|'+ data['company_industry'] +'|'+ data['company_financing'] +'|'+ data['company_size']
+                self.write_csv(str_data)
 
 
 
@@ -49,7 +88,6 @@ if __name__ == '__main__':
     category_list = ['/c101010100-p100101/', '/c101010100-p100102/', '/c101010100-p100103/']
     for category in category_list:
         BossSpider().get_category_info(category)
-
 
 
 
